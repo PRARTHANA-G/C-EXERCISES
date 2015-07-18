8efdc8d8edef42736e759c823a01ea5b80403bb7
minhnh1001 <minhnh1001@gmail.com>
2015-07-18 23:50:52 +0700

    Update array6.cpp

--- array6.cpp
+++ array6.cpp
@@ -22,21 +22,16 @@ void displaypascal(int size)
 	
 	printf("PASCAL TRIANGLE (size = %d)\n", size);
 	
-	//assign 0 to every array element
+	//assign 1 to every array element
 	for (i=0;i,i<size;i++){
-		for (j=0;j<size;j++) pascaltriangle[i][j]=0;}
+		for (j=0;j<size;j++) pascaltriangle[i][j]=1;}
 
-	//set first and second row
-	pascaltriangle[0][0]=1;
-	pascaltriangle[1][0]=1;
-	pascaltriangle[1][1]=1;
-
-	//assig the rest of the array
+	//assign the rest of the array
 	for (i=2;i,i<size;i++){
 		pascaltriangle[i][0]=1;
-		for (j=1;j<size;j++) {pascaltriangle[i][j]=pascaltriangle[i-1][j-1]+pascaltriangle[i-1][j];}}
+		for (j=1;j<i;j++) {pascaltriangle[i][j]=pascaltriangle[i-1][j-1]+pascaltriangle[i-1][j];}}
 	
-
+	//print the pascaltriangle
 	for (i=0;i<size;i++)
 	{
 		for (j=0;j<=i;j++) printf("%d\t", pascaltriangle[i][j]);
@@ -44,8 +39,6 @@ void displaypascal(int size)
 	}
 }
 	
-
-
 int main()
 {
 	int size;
