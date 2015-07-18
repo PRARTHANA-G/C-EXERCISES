0a7d7538beea931cc8aee35364f731857bef66e4
minhnh1001 <minhnh1001@gmail.com>
2015-07-18 17:45:31 +0700

    use array (compared array04)
    /*  By using two-dimensional array, write C program to display a table
    of numbers as shown below:
    
    1 2 3 4 5
    6 7 8  9 10
    11 12 13 14 15
    16 17 18 19 20
    21 22 23 24 25*/

---
+++ array4.cpp
@@ -0,0 +1,31 @@
+/*  By using two-dimensional array, write C program to display a table of numbers as shown below: 
+
+1	2	3	4	5	 
+6	7	8 	9	10
+11	12	13	14	15
+16	17	18	19	20
+21	22	23	24	25*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+
+	int i, j;
+	int temp=1;
+		
+	for (i=0;i<5;i++)
+	{
+		for (j=0;j<5;j++) 
+		{
+			a[i][j]=temp;
+			printf("%d", a[i][j]); printf("\t"); temp=temp+1;
+		}
+		temp=temp;
+		printf("\n");
+		
+	}
+system("PAUSE");
+return 0;
+}
